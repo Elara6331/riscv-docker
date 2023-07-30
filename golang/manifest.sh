968bee5406055b6ed1dfb0e1543aea805f2e57c8
Elara Musayelyan <elara@elara.ws>
2023-07-30 12:25:21 -0700

    Finish manifest.sh for golang

--- golang/manifest.sh
+++ golang/manifest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+docker login gitea.elara.ws
 docker manifest create gitea.elara.ws/elara6331/golang:latest \
 	--amend gitea.elara.ws/elara6331/golang:amd64 \
 	--amend gitea.elara.ws/elara6331/golang:arm64 \
 	--amend gitea.elara.ws/elara6331/golang:riscv64
+docker manifest push gitea.elara.ws/elara6331/golang:latest
